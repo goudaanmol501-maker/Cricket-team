97db3723cbf2fc4df73d338f4eaafefee56116f5
goudaanmol501-maker <goudaanmol501@gmail.com>
2026-05-23 15:38:25 +0530

    commets for changes

--- src/app/teams.ts
+++ src/app/teams.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class TeamsService {
   private teams = [
-    { id: 'team-a', name: 'Team A', players: ['Alice', 'Bob', 'Charlie'] },
-    { id: 'team-b', name: 'Team B', players: ['Dan', 'Eve', 'Frank'] },
+    { id: 'team-a', name: 'Team A', players: ['Anmol', 'Ganesh', 'Sudeep'] },
+    { id: 'team-b', name: 'Team B', players: ['Rohan', 'Roshan', 'Rovan'] },
   ];
 
   getTeams(): any[] {
